d9bd0dc02a4ac5f71d4655e5759cae0266d5e5fd
sddhrthrt <sddhrthrt@gmail.com>
2013-05-07 01:43:01 +0530

    added moving toward

--- js/flocklib.js
+++ js/flocklib.js
@@ -1,6 +1,9 @@
+var varc = 1;
+var vsrc = 0.9;
+var vcrc = 0.001;
 function distance(x1, y1, x2, y2){
-    xd = parseInt(x1)-parseInt(x2);
-    yd = parseInt(y1)-parseInt(y2);
+    xd = parseFloat(x1)-parseFloat(x2);
+    yd = parseFloat(y1)-parseFloat(y2);
     dist = Math.sqrt(xd*xd+yd*yd);
     return dist;
 }
@@ -9,12 +12,28 @@ function divDistance(div1, div2){
 }
 function interact(world){
     _.each(world.birds, function(bird){
+        /* Now, going to each bird, we have to run the following algorithm. 
+         * We are going to find out var, vsr, vcr, vsim and vdsim.
+         * We'll add them up to make a nice new velocity,
+         * then assign them to the bird.
+         */
+        /* First, 'filter' birds for close ones
+         */
         var selection = _.filter(world.birds, function(b){ return divDistance(b, bird)<50; });
         var velocityar = [0.0, 0.0];
+        var velocitycr = [0.0, 0.0];
         _.each(selection, function(b){
+            /* var = (1/n)E(i=1->n)vi
+             *        1   n   
+             * V   = ---  <   V
+             *  ar    n   <    i
+             *           i=1
+             */
             var v = world.getVelocity(b);
             velocityar[0] += v[0];
             velocityar[1] += v[1];
+            velocitycr[0] += parseFloat(b.style.left)-parseFloat(bird.style.left);
+            velocitycr[1] += parseFloat(b.style.top)-parseFloat(bird.style.top);
         });
         velocityar[0] = velocityar[0]/selection.length;
         velocityar[1] = velocityar[1]/selection.length;
@@ -22,6 +41,13 @@ function interact(world){
         var velocitysr = [0.0, 0.0];
         var vc = world.getVelocity(bird);
         _.each(selection, function(b){
+            /* vsr = E(i=1->n)(vi-vc)/d(Fi,Ac)
+             *           ________
+             *        n  V  + V
+             * V   =  <   i    c
+             *  ar    < ----------- 
+             *       i=1 d(Fi, Ac)
+             */
             var v = world.getVelocity(b),
                 d = divDistance(b, bird);
             if(d!=0){
@@ -29,11 +55,11 @@ function interact(world){
                 velocitysr[1] += (0-(v[1]+vc[1]))/d;
             }
         });
-        world.velocities[_.indexOf(world.birds, bird)][0] = velocityar[0];
-        world.velocities[_.indexOf(world.birds, bird)][1] = velocityar[1];
-        if(velocitysr[0]>0 && velocity[1]>0){
-            world.velocities[_.indexOf(world.birds, bird)][0] = velocityar[0]*0.2+velocitysr[0]*0.8;
-            world.velocities[_.indexOf(world.birds, bird)][1] = velocityar[1]*0.2+vecolitysr[1]*0.8;
+        world.velocities[_.indexOf(world.birds, bird)][0] = velocityar[0]*varc+velocitycr[0]*vcrc;
+        world.velocities[_.indexOf(world.birds, bird)][1] = velocityar[1]*varc+velocitycr[1]*vcrc;
+        if(velocitysr[0]>0 && velocitysr[1]>0){
+            world.velocities[_.indexOf(world.birds, bird)][0] = velocityar[0]*varc+velocitysr[0]*vsrc+velocitycr[0]*vcrc;
+            world.velocities[_.indexOf(world.birds, bird)][1] = velocityar[1]*varc+velocitysr[1]*vsrc+velocitycr[0]*vcrc;
         }
     });
 }
