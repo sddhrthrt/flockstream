2b936653923f07370111d69a3246dc420a25e793
sddhrthrt <sddhrthrt@gmail.com>
2013-05-06 20:28:16 +0530

    yayy. clustering, without separating.

--- js/flocklib.js
+++ js/flocklib.js
@@ -1,6 +1,39 @@
-function interact(birds){
-    _.each(birds, function(bird){
-        var selection = _.filter(birds, function(b){ return areClose(b.x, b.y, bird.x, bird.y); });
-        console.log('for', bird, ':', selection);
+function distance(x1, y1, x2, y2){
+    xd = parseInt(x1)-parseInt(x2);
+    yd = parseInt(y1)-parseInt(y2);
+    dist = Math.sqrt(xd*xd+yd*yd);
+    return dist;
+}
+function divDistance(div1, div2){
+    return distance(div1.style.left, div1.style.top, div2.style.left, div2.style.top);
+}
+function interact(world){
+    _.each(world.birds, function(bird){
+        var selection = _.filter(world.birds, function(b){ return divDistance(b, bird)<50; });
+        var velocityar = [0.0, 0.0];
+        _.each(selection, function(b){
+            var v = world.getVelocity(b);
+            velocityar[0] += v[0];
+            velocityar[1] += v[1];
+        });
+        velocityar[0] = velocityar[0]/selection.length;
+        velocityar[1] = velocityar[1]/selection.length;
+        var selection = _.without(_.filter(world.birds, function(b){ return divDistance(b, bird)<10; }), bird);
+        var velocitysr = [0.0, 0.0];
+        var vc = world.getVelocity(bird);
+        _.each(selection, function(b){
+            var v = world.getVelocity(b),
+                d = divDistance(b, bird);
+            if(d!=0){
+                velocitysr[0] += (0-(v[0]+vc[1]))/d;
+                velocitysr[1] += (0-(v[1]+vc[1]))/d;
+            }
+        });
+        world.velocities[_.indexOf(world.birds, bird)][0] = velocityar[0];
+        world.velocities[_.indexOf(world.birds, bird)][1] = velocityar[1];
+        if(velocitysr[0]>0 && velocity[1]>0){
+            world.velocities[_.indexOf(world.birds, bird)][0] = velocityar[0]*0.2+velocitysr[0]*0.8;
+            world.velocities[_.indexOf(world.birds, bird)][1] = velocityar[1]*0.2+vecolitysr[1]*0.8;
+        }
     });
 }
--- js/ourcode.js
+++ js/ourcode.js
@@ -1,36 +1,40 @@
 var world  = new Object();
 world.width = 1200;
 world.height = 661;
-var values = [],
-    velocities = [],
-    pos = [];
-
+world.values = [];
+world.velocities = [];
+world.pos = [];
+world.getVelocity = function(bird){
+    return world.velocities[_.indexOf(world.birds, bird)];
+}
 function step(){
     _.each(world.birds, move);
 }
+
 function move(bird){
     id = parseInt(bird.id);
-    newx = parseInt(bird.style.left) + Math.floor(velocities[id][0]);
-    newy = parseInt(bird.style.top) + Math.floor(velocities[id][1]);
+    newx = parseInt(bird.style.left) + Math.floor(world.velocities[id][0]);
+    newy = parseInt(bird.style.top) + Math.floor(world.velocities[id][1]);
     newx = newx<0?world.width:newx;
     newx = newx>world.width?0:newx;
     newy = newy<0?world.height:newy;
     newy = newy>world.height?0:newy;
     bird.style.left = newx+'px';
     bird.style.top = newy+'px';
+    interact(world);
 }
 function randomize(){
     //Generate random values for these things.
-    values = [];
-    velocities = [];
-    pos = [];
+    world.values = [];
+    world.velocities = [];
+    world.pos = [];
     velocity_unit = 10;
-    for(i=0;i<50;i++) values.push(Math.random());
+    for(i=0;i<50;i++) world.values.push(Math.random());
     for(i=0;i<50;i++){
-        pos.push([Math.random()*world.width, Math.random()*world.height]);
+        world.pos.push([Math.random()*world.width, Math.random()*world.height]);
     }
     for(i=0;i<50;i++){
-        velocities.push([Math.random()*velocity_unit-velocity_unit/2, Math.random()*velocity_unit-velocity_unit/2]);
+        world.velocities.push([Math.random()*velocity_unit-velocity_unit/2, Math.random()*velocity_unit-velocity_unit/2]);
     }
 }
 function setUp(world){
@@ -38,13 +42,12 @@ function setUp(world){
     var sky = document.getElementById('sky');
     randomize();
     var counter = 0;
-    _.each(pos, function(position){
+    _.each(world.pos, function(position){
         sky.innerHTML += "<div class='black bird' id="+counter+" style='left:"+position[0]+";top:"+position[1]+";'></div>";
         counter += 1;
     });
     var birds = document.getElementsByClassName('bird');
     world.birds = birds;
-    interact(world.birds);
 }
 
 //setUp(world);
